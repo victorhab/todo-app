389f8467772010162eb4309ee14da6f5af0e6a03
victorhab <victorhaby10@gmail.com>
2017-12-22 08:42:52 +0100

    fixed site, just need login

---
+++ todo/static/js/slide.js
@@ -0,0 +1,5 @@
+$(document).ready(() => {
+    $('#loginSlide').on('click', () => {
+        $('#loginBar').slideToggle();
+    });
+});
--- todo/views.py
+++ todo/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Activity
-from django.shortcuts import render, get_object_or_404
+from .forms import ActivityForm
+from django.shortcuts import redirect
+
 
 # Create your views here.
 def activity_list(request):
-    activities = Activity.objects.all()
+    #activities = Activity.objects.all()
+    if not request.user.is_authenticated:
+        activities = None
+    else:
+        activities = Activity.objects.filter(author=request.user)
     return render(request, 'todo/activity_list.html', {'activities': activities})
 
 def activity_detail(request, pk):
     activity = get_object_or_404(Activity, pk=pk)
     return render(request, 'todo/activity_detail.html', {'activity': activity})
+
+def add_new(request):
+    if request.method == "POST":
+        form = ActivityForm(request.POST)
+        if form.is_valid():
+            activity = form.save(commit=False)
+            activity.author = request.user
+            activity.save()
+            return redirect('activity_detail', pk=activity.pk)
+
+    else:
+        form = ActivityForm()
+    return render(request, 'todo/activity_edit.html', {'form': form})
